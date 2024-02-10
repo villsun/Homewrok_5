15b9334700a80586db65ba0f1a693623d67e4106
villsun <lonukkola@gmail.com>
2024-02-10 23:12:39 +0100

    Search_Brackets_Square

--- Lesson_5(homework)/Homework.cpp
+++ Lesson_5(homework)/Homework.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string.h>
 
 using namespace std;
@@ -23,9 +23,39 @@ bool Search_Brackets(char* str)
 	}
 	return counter == 0;
 }
+
+int Search_Brackets_Square(char* str)
+{
+	const int SIZE = strlen(str);
+	char* p = new char[SIZE];
+	int top = -1;
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (str[i] == '(' || str[i] == '[')
+		{
+			p[++top] = str[i];
+		}
+		else if (str[i] == ')' || str[i] == ']')
+		{
+			if (top == -1)
+			{
+				return 0;
+			}
+			char topChar = p[top--];
+			if ((str[i] == ')' && topChar != '(') || (str[i] == ']' && topChar != '['))
+			{
+				return 0;
+			}
+		}
+	}
+	delete[] p;
+
+	return top;
+}
+
 int main()
 {
-	// ???????? 1;
+	// Завдання 1;
 
 	/*char str[] = "He(llo)";*/
 	/*if (Search_Brackets(str))
@@ -38,6 +68,11 @@ int main()
 	}*/
 
 	/*Search_Brackets(str) ? cout << "All is well\n" : cout << "Not all is well\n";*/
+
+	char str[] = "He([llo)]";
+
+	Search_Brackets_Square(str) == -1 ? cout << "All is well\n" : cout << "Not all is well\n";
+
 
 	return 0;
 }
